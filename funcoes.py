97cfc76457107fe71d17c72a01f1dccbb9e6688f
Joel Costa <joel.costa@m4ucorp.dmc>
2020-04-26 15:04:58 -0300

    Including try/catch in caesar_encrypt

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,4 @@
+# Given a integer n, returns the nth term of the fibonacci sequence
 def fibonacci (x):
     try:
         if x > 1:
@@ -11,18 +12,28 @@ def fibonacci (x):
     except:
         return 0
 
+# Encode a given string with ceaser cipher method, in which each letter in the realtext is replaced 
+# by a letter some fixed number of positions (steps) down the alphabet.
 def caesar_encrypt(realText, step):
-    ucase='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    lcase= ucase.lower()
+    try:
+        if type(realText) != str or type(step) != int:
+            raise TypeError
 
-    cipherText = ''
+        ucase='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        lcase= ucase.lower()
 
-    for c in realText:
-        if c.isalpha():
-            if c in ucase:
-                cipherText += ucase[(ucase.index(c)+step)%26]
+        cipherText = ''
+
+        for c in realText:
+            if c.isalpha():
+                if c in ucase:
+                    cipherText += ucase[(ucase.index(c)+step)%26]
+                else:
+                    cipherText += lcase[(lcase.index(c)+step)%26]
             else:
-                cipherText += lcase[(lcase.index(c)+step)%26]
-        else:
-            cipherText += c
-    return cipherText
+                cipherText += c
+
+        return cipherText
+
+    except TypeError:
+        print('Parâmetros inválidos: realText deve ser string e step inteiro')
